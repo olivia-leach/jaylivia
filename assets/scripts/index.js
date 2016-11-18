a3c02b319a7b705cd11453832e10f29b9748d8c2
olivia-leach <olivia.i.m.leach@gmail.com>
2016-11-18 10:52:27 -0500

    Add video

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,41 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+$('#videoButton').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $('.video').offset().top
+  }, 1000);
+})
+
+$('#playButton').on('click', function(e) {
+
+  $(this).hide()
+  $('#loading').show();
+
+  var player = new YT.Player('theVideo', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange,
+        }
+    });
+
+  function onPlayerReady(event) {
+    player.playVideo();
+  }
+  function onPlayerStateChange(event) {
+    console.log(event.data);
+    if (event.data === 1) {
+      $('.video-cover').hide();
+    }
+    if (event.data === 0) {
+      // player.seekTo(0);
+      player.stopVideo();
+      $('#playButton').show()
+      $('#loading').hide();
+      $('.video-cover').fadeIn();
+    }
+  }
+
+})
